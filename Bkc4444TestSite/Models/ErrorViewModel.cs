b21afb547efd6c16c20572e06323c84f0abbe987
Brandon Carroll <bkc4444@gmail.com>
2022-11-27 13:45:45 -0800

    Add project files.

---
+++ Bkc4444TestSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bkc4444TestSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
